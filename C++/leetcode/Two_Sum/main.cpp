be0bfd206301e74d8edc85bf7f247400349911c2
chengyi818 <chengyi818@foxmail.com>
2016-04-21 10:11:51 +0800

    [leetcode] update source

--- C++/leetcode/Two_Sum/main.cpp
+++ C++/leetcode/Two_Sum/main.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ret;
-        int slot;
-        class hash_node *pTail;
+        int slot,target_left;
+        hash_node *pTail,pTemp;
 
         //hash table init
-        class hash_node *hash_table[100];
+        hash_node *hash_table[100];
         for(int i = 0; i < 100; i++)
             hash_table[i] = NULL;
         for(vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
         {
             slot = nums[it] % 100;
-            class hash_node *pTemp = new(class hash_node);
+            pTemp = new(hash_node);
             pTemp->data = nums[it];
+            pTemp->index = it;
             pTemp->pNext = NULL;
 
             if(NULL == hash_table[slot])
@@ -31,16 +32,31 @@ public:
             }
         }
 
-        //search
+        //search loop
+        for(vector<int>::iterator it = nums.begin(); it != nums.end(); it++)
+        {
+            target_left = target-nums[it];
+            slot = target_left%100;
+            pTemp = hash_table[slot];
 
+            if( NULL == pTemp )
+                continue;
 
-
-
-        return ret;
+            for(;NULL != pTemp;pTemp = pTemp->pNext)
+            {
+                 if(pTemp->data == target_left )
+                 {
+                    ret.push_back(it);
+                    ret.push_back(pTemp->index);
+                    return ret;
+                 }
+            }
+        }
     }
 private:
-    class hash_node {
+    typedef struct Hash_Node {
         int data;
-        class hash_node *pNext;
-    };
+        int index;
+        struct Hash_Node *pNext;
+    } hash_node;
 };
