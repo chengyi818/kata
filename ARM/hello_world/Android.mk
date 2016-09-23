d8a886509a19c56c1d41d22aab79afdcc5390252
chengyi <chengyi@antiy.cn>
2016-09-23 14:52:08 +0800

    Develop | update hello world

--- ARM/hello_world/Android.mk
+++ ARM/hello_world/Android.mk
@@ -1,8 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_ARM_MODE := arm
-LOCAL_MODULE := hello_world
 LOCAL_SRC_FILES := hello_world.c
+LOCAL_ARM_MODE := arm
+
+LOCAL_MODULE := hello_world
 
 include $(BUILD_SHARED_LIBRARY)
+#include $(BUILD_EXECUTABLE)
+
+
+
--- ARM/hello_world/hello_world.c
+++ ARM/hello_world/hello_world.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 int main(int argc, char *argv[])
 {
-    printf("Hello ARM!\n");
+    printf("Hello World!\n");
     return 0;
 }
