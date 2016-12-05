e330e1af51e54e7d8a1f51016eb35efd2df0953f
chengyi <chengyi@antiy.cn>
2016-12-05 23:00:25 +0800

    Python | Add code for copy

---
+++ python/copy/copy.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+# =============================================================================
+#      FileName: assign.py
+#          Desc:
+#        Author: ChengYi
+#         Email: chengyi@antiy.cn
+#      HomePage: http://www.antiy.com/
+#       Created: 2016-12-05 22:49:15
+#       Version: 0.0.1
+#    LastChange: 2016-12-05 22:49:15
+#       History:
+#                0.0.1 | ChengYi | init
+# =============================================================================
+'''
+import copy
+
+def my_print(obj):
+    print id(obj)
+    print obj
+    print [id(ele) for ele in obj]
+    print "\n"
+
+
+will = ["Will", 28, ["Python", "C++", "C"]]
+wilber = copy.copy(will)
+
+my_print(will)
+my_print(wilber)
+
+will[0] = "Wilber"
+will[2].append("Java")
+my_print(will)
+my_print(wilber)
+
