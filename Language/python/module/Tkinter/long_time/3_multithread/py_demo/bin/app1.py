ad438632f311c9e7aaf67985203914b2f3ddc10f
chengyi818 <chengyi818@foxmail.com>
2017-07-18 20:11:04 +0800

    Kata | python: update multithread demo of Tkinter

--- Language/python/module/Tkinter/long_time/3_multithread/py_demo/bin/app1.py
+++ Language/python/module/Tkinter/long_time/3_multithread/py_demo/bin/app1.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python3
-# Author: ChengYi
-# Mail: chengyi818@foxmail.cn
-# created time: Thu 29 Jun 2017 08:19:21 PM CST
-
 import logging
 import time
 import gettext
 _ = gettext.gettext
 
-
 def calc():
-    logger = logging.getLogger(__name__)
-    for i in range(100, 0, -1):
-        logger.info(_('Countdown value is %i'), i)
-        time.sleep(0.2)
-
-    logger.info(_('Done!'))
-
+  logger = logging.getLogger(__name__)
+  for i in range(100, 0, -1):
+    logger.info (_('Countdown value is %i'), i)
+    time.sleep(.2)
+  logger.info(_('Done!'))
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
