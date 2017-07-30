eba343822c0cf072c5c77b98c6ba5f4d488d8165
chengyi818 <chengyi818@foxmail.com>
2017-07-30 23:09:45 +0800

    update python encode

--- Language/python/grammer/encode/demo1.py
+++ Language/python/grammer/encode/demo1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-TEST = '0000测试0000'
+TEST = '0000中0000'
 print(type(TEST))
+print(len(TEST))
 TEST = TEST.encode('utf-8')
 
 print(type(TEST))
-print([TEST])
+print(TEST)
 print(len(TEST))
