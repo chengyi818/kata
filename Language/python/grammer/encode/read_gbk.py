1583aaec647df952daad25c955e5493ce7f20660
chengyi818 <chengyi818@foxmail.com>
2017-08-29 20:28:21 +0800

    Python | encode: add read_gbk.py

--- Language/python/grammer/encode/read_gbk.py
+++ Language/python/grammer/encode/read_gbk.py
@@ -7,9 +7,9 @@ with open("text_gbk.txt", "wb+") as file_handle:
     file_handle.write(content_gbk)
 
 with open("text_gbk.txt", "r") as file_handle:
-    # print("hello world")
-    content_read = file_handle.read()
-    print(content_read)
+    print("hello world")
+    # content_read = file_handle.read()
+    # print(content_read)
 
 with codecs.open("text_gbk.txt", "r", 'gbk') as file_handle:
     content_read = file_handle.read()
