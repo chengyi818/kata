5f029d820541e2b3564803516456a2cd01df66dd
chengyi818 <chengyi818@foxmail.com>
2017-08-30 11:20:14 +0800

    Python | encoding: upadte read_gbk.py

--- Language/python/grammer/encode/read_gbk.py
+++ Language/python/grammer/encode/read_gbk.py
@@ -6,10 +6,10 @@ with open("text_gbk.txt", "wb+") as file_handle:
     content_gbk = content.encode("gbk")
     file_handle.write(content_gbk)
 
-with open("text_gbk.txt", "r") as file_handle:
-    print("hello world")
-    # content_read = file_handle.read()
-    # print(content_read)
+with open("text_gbk.txt", "r", encoding='gbk') as file_handle:
+    # print("hello world")
+    content_read = file_handle.read()
+    print(content_read)
 
 with codecs.open("text_gbk.txt", "r", 'gbk') as file_handle:
     content_read = file_handle.read()
