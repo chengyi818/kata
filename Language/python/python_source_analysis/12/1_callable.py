4956b01f9c219183b7e6cefee634a73d043fdb4a
chengyi818 <chengyi818@foxmail.com>
2018-12-19 18:11:46 +0800

    Maintain | Algorithm: Add Algorithm directory

--- Language/python/python_source_analysis/12/1_callable.py
+++ Language/python/python_source_analysis/12/1_callable.py
@@ -2,6 +2,10 @@ class A(object):
     def __call__(self):
         print("A called")
 
+    def f(self):
+        print("foo")
+
 
 a = A()
+a.f()
 a()
---
+++ Language/python/python_source_analysis/12/2_not_callable.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author: ChengYi
+# Mail: chengyi818@foxmail.cn
+# created time: Thu 06 Dec 2018 02:56:15 PM CST
+
+
+def f():
+    i = 1
+    i()
+
+
+f()
