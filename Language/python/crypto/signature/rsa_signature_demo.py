7da6f57ec6a5b91abf492fb9eee909c56321cf3a
chengyi818 <chengyi818@foxmail.com>
2019-05-30 20:08:22 +0800

    Develop | Python: add comment for crypto

--- Language/python/crypto/signature/rsa_signature_demo.py
+++ Language/python/crypto/signature/rsa_signature_demo.py
@@ -3,16 +3,18 @@
 # Mail: chengyi818@foxmail.cn
 # created time: Mon 13 May 2019 02:45:11 PM CST
 
-from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
 
+# sign
 message = 'To be signed'.encode('utf-8')
 key = RSA.import_key(open('private.pem').read())
 h = SHA256.new(message)
 signature = pkcs1_15.new(key).sign(h)
 print("signature: ", signature)
 
+# verify
 key = RSA.import_key(open('receiver.pem').read())
 h = SHA256.new(message)
 try:
